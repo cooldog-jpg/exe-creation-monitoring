f94c629056b11c8d6cb90885b17640683dd3f1bb
cooldog-jpg <cooldog_jpg@protonmail.com>
2016-09-03 03:53:33 -0600

    Updated script.ps1
    Fixed issues with variables scope

--- script.ps1
+++ script.ps1
@@ -6,33 +6,57 @@
 #       cooldog_jpg@protonmail.com              #
 #===============================================#
 
-#Define constants and retrieve environment variables
-$path = $env:appdata 
-$filename = "*.exe"
-$destination = $env:userprofile
-$logfile = $env:userprofile\files.log #Create the file before running the script
-$client = $env:computername
-
 # Initialize the Watcher object
 $Watcher = New-Object System.IO.FileSystemWatcher 
 
+#Define constants and retrieve environment variables
+$filename = "*.exe"
+$global:Destination_drive = "C:\" # Change this
+
 # Setting properties for the watcher object
-$Watcher.Path = $path
+$Watcher.Path = $env:appdata # Change this
 $Watcher.Filter = $filename
-$Watcher.NotifyFilter = [IO.NotifyFilters]"FileName"
+$Watcher.NotifyFilter = [IO.NotifyFilters]"FileName, LastWrite"
 $Watcher.IncludeSubdirectories = $False
 
 # Define the action to be performed after the binary creation
 Register-ObjectEvent $Watcher Created -SourceIdentifier FileCreated -Action {
-    $name = $Event.SourceEventArgs.Name
-    $changeType = $Event.SourceEventArgs.ChangeType
-    $timeStamp = $Event.TimeGenerated
-    $hash = Get-FileHash "$path\$name" -Algorithm MD5
-    $md5 = md5.hash
-    Write-Host $client ": The file '$name' was $changeType at $timeStamp" -fore green
-    Out-File -FilePath $logfile -Append -InputObject "$client : The file '$name' was $changeType at $timeStamp | MD5: $md5"
-    Write-Host "Copying $path\$name to $destination\$client_$name"
-    Copy-Item -Path "$path\$name" -Destination "$destination\$client_$name" -Force -Verbose
+	$name = $Event.SourceEventArgs.Name
+	$changeType = $Event.SourceEventArgs.ChangeType
+	$timeStamp = $Event.TimeGenerated
+	
+	$client = $env:computername
+	$fileLocation = "$env:appdata\$name"
+	$destination = "$global:Destination_drive\$client"
+	$logfile = "$destination\log.txt" # Change this if you want
+
+	Write-Host "=== New binary detected in $destination ===" -back white -fore black
+	
+	# Create directory and log file
+	if (!(Test-Path $destination)){
+		New-Item $destination -itemtype directory
+		Out-File -FilePath $logfile -Append -InputObject "New folder and log file created for $client"
+	}
+
+	if (!(Test-Path "$destination\log.txt")){
+		New-Item "$destination\log.txt" -itemtype file
+	}
+	
+	# Collect hash and sample and copy binary to remote drive
+	Try {
+		$fp = Get-FileHash $fileLocation
+		$hash = $fp.hash
+	}
+	Catch
+	{
+		$ErrorMessage = $_.Exception.Message
+		$FailedItem = $_.Exception.ItemName
+		$hash = $ErrorMessage
+	}
+	Write-Host $client ": The file $name was $changeType at $timeStamp" -fore green
+	Out-File -FilePath $logfile -Append -InputObject "$client : The file '$name' was $changeType at $timeStamp | Hash: $hash"
+	Write-Host "Copying $fileLocation to $destination"
+	Copy-Item -Path $fileLocation -Destination $destination -Force -Verbose
 }
 
 #EOF
